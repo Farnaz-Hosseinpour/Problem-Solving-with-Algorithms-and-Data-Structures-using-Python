e7e117828f86da86783224a624a850e69f08d0c4
Scientist <e.erfani81@gmail.com>
2020-05-16 19:54:39 -0700

    Add files via upload

---
+++ Chapter 7. Graphs and Graph Algorithms/dijkstra/dijkstra dense graphs/vertex.py
@@ -0,0 +1,6 @@
+
+class Vertex:
+    def __init__(self, label: str = None, weight: int = None, index: int = None):
+        self.label = label
+        self.weight = weight
+        self.index = index
